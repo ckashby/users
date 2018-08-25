23c83b11999b176b2fd716f1b8ab91b2367a1bb3
ckashby <clay.ashby@gmail.com>
2018-08-24 17:39:35 -0700

    Added Tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = current_user.tasks.build
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to tasks_path   #TODO: show
+    else
+      render :new
+    end
+  end
+
+  def show
+  end 
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'Task successfully updated'  #TODO: show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: 'Task successfully deleted'
+  end
+
+
+  private
+
+    def find_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :user_id)
+    end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :pets
+  has_many :tasks
+  
 end
